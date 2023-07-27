4e2b70b58357ac3ce17247e87d9cdaf4f467a84c
Natthasak Vechprasit <9090871+babebbu@users.noreply.github.com>
2023-07-27 14:37:32 +0700

    Create app.js

---
+++ app.js
@@ -0,0 +1,36 @@
+const WebSocket = require('ws');
+
+// Replace 'ws://your-websocket-server-url' with the actual WebSocket server URL
+const webSocketUrl = 'ws://172.16.10.188:8080/ws/app/status-annouuncement';
+
+const ws = new WebSocket(webSocketUrl);
+
+// WebSocket 'open' event: when the connection is established
+ws.on('open', () => {
+  console.log('WebSocket client connected.');
+
+  // Send a message to the WebSocket server
+  // ws.send('Hello, WebSocket server!');
+});
+
+// WebSocket 'message' event: when a message is received from the server
+ws.on('message', (message) => {
+  console.log(`Received message from server: ${message}`);
+});
+
+// WebSocket 'error' event: when an error occurs
+ws.on('error', (error) => {
+  console.error(`WebSocket error: ${error.message}`);
+});
+
+// WebSocket 'close' event: when the connection is closed
+ws.on('close', (code, reason) => {
+  console.log(`WebSocket connection closed: code=${code}, reason=${reason}`);
+});
+
+// Gracefully close the WebSocket connection when the process is terminated
+process.on('SIGINT', () => {
+  console.log('Closing WebSocket connection...');
+  ws.close();
+  process.exit();
+});
